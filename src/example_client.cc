a4608c3edd469e6e595038ac2de9025a012014b1
Christopher Cifra <christopher.cifra@ni.com>
2021-05-07 14:52:20 -0500

    fix build issue

--- src/example_client.cc
+++ src/example_client.cc
@@ -151,32 +151,25 @@ string GetServerAddress(int argc, char** argv)
     string arg_str("--target");
     if (argc > 1)
     {
-        if (UseProxy(argc, argv))
+        string arg_val = argv[1];
+        size_t start_pos = arg_val.find(arg_str);
+        if (start_pos != string::npos)
         {
-            target_str = "localhost";
-        }
-        else
-        {
-            string arg_val = argv[1];
-            size_t start_pos = arg_val.find(arg_str);
-            if (start_pos != string::npos)
+            start_pos += arg_str.size();
+            if (arg_val[start_pos] == '=')
             {
-                start_pos += arg_str.size();
-                if (arg_val[start_pos] == '=')
-                {
-                    target_str = arg_val.substr(start_pos + 1);
-                }
-                else
-                {
-                    cout << "The only correct argument syntax is --target=" << endl;
-                    return 0;
-                }
+                target_str = arg_val.substr(start_pos + 1);
             }
             else
             {
-                cout << "The only acceptable argument is --target=" << endl;
+                cout << "The only correct argument syntax is --target=" << endl;
                 return 0;
             }
+        }
+        else
+        {
+            cout << "The only acceptable argument is --target=" << endl;
+            return 0;
         }
     }
     else
